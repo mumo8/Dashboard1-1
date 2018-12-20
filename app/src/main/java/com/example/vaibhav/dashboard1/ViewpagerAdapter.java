88df8ab85e2cab90e9066ba85c00451ff5a83481
wsdesign <info.wsdesignuiux@gmail.com>
2018-12-20 11:03:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vaibhav/dashboard1/ViewpagerAdapter.java
@@ -0,0 +1,35 @@
+package com.example.vaibhav.dashboard1;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+
+public class ViewpagerAdapter extends FragmentStatePagerAdapter {
+    public ViewpagerAdapter(FragmentManager fm) {
+
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position)
+        {
+            case 0:
+                Viewpager1 tab1 = new Viewpager1();
+                return tab1;
+            case 1:
+                Viewpager2 tab2 = new Viewpager2();
+                return  tab2;
+                default:
+                    return null;
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
